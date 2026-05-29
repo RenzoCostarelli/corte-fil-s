095e6678f18b65b51f56738c7d1f8c57350a37fa
Renzo Costarelli <renzo.costarelli@gmail.com>
2026-05-29 16:15:31 -0300

    Agrega proyectos y animaciones

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -3,6 +3,7 @@ export interface PortfolioItem {
   name: string;
   category: string;
   year: number;
+  thumbnail?: string;
   roles: string[];
   description: string;
   longDescription: string;
@@ -10,26 +11,30 @@ export interface PortfolioItem {
 
 export const portfolio: PortfolioItem[] = [
   {
-    slug: "proyecto-social-1",
-    name: "Proyecto Social 1",
-    category: "Social",
-    year: 2026,
-    roles: ["Dirección", "Edición", "Postproducción"],
+    slug: "grupo-crriolani",
+    name: "Grupo Criolani",
+    category: "Institucional",
+    year: 2025,
+    roles: ["Guión", "Dirección", "Edición"],
+    thumbnail:
+      "https://res.cloudinary.com/dkgnaegp9/image/upload/v1780081150/Criolani_rpy1ga.png",
     description:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi totam facilis necessitatibus nostrum, fuga earum, nulla ipsam nam provident reiciendis aliquam. Vitae perspiciatis ullam culpa, cumque beatae officiis.",
     longDescription:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi totam facilis necessitatibus nostrum, fuga earum, nulla ipsam nam provident reiciendis. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi totam facilis necessitatibus nostrum, fuga earum, nulla ipsam nam provident reiciendis.",
+      "Realizamos este video institucional para Grupo Criolani, una de las concesionarias de maquinaria agrícola más importantes del país, con 6 sucursales en 3 provincias. Trabajamos en el guión junto a representantes del área comercial y marketing de la empresa buscando destacar el tamaño, la trayectoria y la calidad del servicio a través de la historia familiar. Al equipo se sumaron Baltazar Lamas, Andrés Picech y Calfú Martinez.",
   },
   {
-    slug: "proyecto-social-2",
-    name: "Proyecto Social 2",
-    category: "Social",
+    slug: "plaza-club",
+    name: "Plaza Club - Todos somos fundadores",
+    category: "Institucional",
     year: 2025,
-    roles: ["Dirección", "Fotografía"],
+    roles: ["Dirección de fotografía", "Edición"],
+    thumbnail:
+      "https://res.cloudinary.com/dkgnaegp9/image/upload/v1780081150/Captura_l1yigu.png",
     description:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi totam facilis necessitatibus nostrum, fuga earum, nulla ipsam nam provident reiciendis aliquam.",
     longDescription:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi totam facilis necessitatibus nostrum, fuga earum, nulla ipsam nam provident reiciendis. Lorem ipsum dolor sit amet consectetur.",
+      "Video realizado para comunicación interna del club. Nos encomendaron este video para contarle a los socios del club sobre la construcción de un “kinder” en el predio. La idea del cliente era generar un video emotivo, haciendo hincapié en la historia y los valores del club. Guionado y dirigido por Juan Mangiantini. Baltazar Lamas en cámara.",
   },
   {
     slug: "videoclip-uno",
